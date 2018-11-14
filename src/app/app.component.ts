73dce4d3548a26bd81489c346799ee404beb1cae
Parham Alvani <parham.alvani@gmail.com>
2018-11-14 21:27:34 +0330

    Completes stopwatch implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'stopwatch';
+  private since: number;
+  private timerID: number;
+  public delta: Date = new Date(0);
+
+  constructor() {
+  }
+
+  private update(): void {
+    this.delta = new Date(performance.now() - this.since);
+  }
+
+  ngOnInit() {
+  }
+
+  public start(): void {
+    this.since = performance.now();
+
+    this.timerID = window.setInterval(
+      this.update.bind(this), 100
+    ); // each 100ms updates UI
+  }
+
+
+  public stop(): void {
+    if (this.timerID) {
+      window.clearInterval(this.timerID);
+    }
+  }
+
+  public reset(): void {
+    this.stop();
+    this.delta = new Date(0);
+  }
 }
